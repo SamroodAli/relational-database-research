f89ce9e35f9b134979517fab87b252301d41e2db
SamroodAli <samrood@peoplegrove.com>
2023-04-15 23:58:31 +0530

    feat: creating a database using the createdb utility

---
+++ create_db.sh
@@ -0,0 +1,9 @@
+# apart from creating databases with the CREATE DATABASE query, we can also use
+# the createdb program
+
+# database names must have alphabetic first character
+# limited to 63 bytes in length
+createdb database_name
+
+# we can also invoke just createdb and enter a db name when prompted
+createdb
